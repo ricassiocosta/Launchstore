d5fcde8cf2f589299b04c74fb4e630cd4e83d9b7
ricassiocosta <riksiocosta@gmail.com>
2020-07-17 11:01:17 -0300

    initial configuration

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg')
+require('dotenv').config()
+
+module.exports = new Pool({
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PSWD,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME
+})
