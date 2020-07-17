7a9ebea6208602de61b026d48a39038efe52b51a
ricassiocosta <riksiocosta@gmail.com>
2020-07-17 13:16:17 -0300

    frontend image manager implemented

--- public/scripts.js
+++ public/scripts.js
@@ -24,3 +24,92 @@ if(formDelete) {
     }
   })
 }
+
+const PhotosUpload = {
+  input: "",
+  preview: document.querySelector('#photos-preview'),
+  uploadLimit: 6,
+  files: [],
+  handleFileInput(event) {
+    const { files: fileList } = event.target
+    PhotosUpload.input = event.target
+    
+    if(PhotosUpload.hasLimit(event)) return
+
+    Array.from(fileList).forEach(file => {
+      PhotosUpload.files.push(file)
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        const image = new Image()
+        image.src = String(reader.result)
+
+        const container = PhotosUpload.getContainer(image)
+        PhotosUpload.preview.appendChild(container)
+      }
+      reader.readAsDataURL(file)
+    })
+
+    PhotosUpload.input.files = PhotosUpload.getAllFiles()
+  },
+
+  hasLimit(event) {
+    const { uploadLimit, input, preview } = PhotosUpload
+    const { files: fileList } = input
+
+    if(fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`)
+      event.preventDefault()
+      return true
+    }
+
+    const photosContainer = []
+    preview.childNodes.forEach(item => {
+      if(item.classList && item.classList.value == 'photo') {
+        photosContainer.push(item)
+      }
+    })
+
+    const totalPhotos = fileList.length + photosContainer.length
+    if(totalPhotos > uploadLimit) {
+      alert('Você excedeu o limite máximo de fotos!')
+      event.preventDefault()
+      return true
+    }
+
+    return false
+  },
+
+  getAllFiles() {
+    const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+    PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+    return dataTransfer.files
+  },
+
+  getContainer(image) {
+    const container = document.createElement('div')
+    container.classList.add('photo')
+    container.onclick = PhotosUpload.removePhoto
+    container.appendChild(image)
+    container.appendChild(PhotosUpload.getRemoveButton())
+
+    return container
+  },
+
+  getRemoveButton() {
+    const button = document.createElement('i')
+    button.classList.add('material-icons')
+    button.innerHTML = 'close'
+    return button
+  },
+
+  removePhoto(event) {
+    const photoContainer = event.target.parentNode
+    const photosArray = Array.from(PhotosUpload.preview.children)
+    const index = photosArray.indexOf(photoContainer)
+    PhotosUpload.files.splice(index, 1)
+    PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+    photoContainer.remove()
+  }
+}
