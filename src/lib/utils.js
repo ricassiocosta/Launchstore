4b9dc9cd87013a3218bb507eaae8e575661723b7
ricassiocosta <riksiocosta@gmail.com>
2020-07-20 18:47:26 -0300

    removing UTC from date functions

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,11 +2,11 @@ module.exports = {
   date(timestamp) {
     const date = new Date(timestamp)
 
-    const year = date.getUTCFullYear()
-    const month = `0${date.getUTCMonth() + 1}`.slice(-2)
-    const day = `0${date.getUTCDate()}`.slice(-2)
-    const hour = date.getUTCHours()
-    const minutes = date.getUTCMinutes()
+    const year = date.getFullYear()
+    const month = `0${date.getMonth() + 1}`.slice(-2)
+    const day = `0${date.getDate()}`.slice(-2)
+    const hour = date.getHours()
+    const minutes = date.getMinutes()
 
     return {
       day,
