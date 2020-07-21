24fed138181739ac07810ed9db333e6b44851421
ricassiocosta <riksiocosta@gmail.com>
2020-07-21 17:24:27 -0300

    switching gallery highlight

--- public/scripts.js
+++ public/scripts.js
@@ -126,3 +126,16 @@ const PhotosUpload = {
     photoContainer.remove()
   }
 }
+
+const ImageGallery = {
+  highlight: document.querySelector('.gallery .highlight > img'),
+  previews: document.querySelectorAll('.gallery-preview img'),
+  setImage(event) {
+    const { target } = event
+
+    ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+    target.classList.add('active')
+
+    ImageGallery.highlight.src = target.src
+  }
+}
