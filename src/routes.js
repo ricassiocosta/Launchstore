81b150474e4613797ab0742dd6661d6580137b34
ricassiocosta <riksiocosta@gmail.com>
2020-08-12 18:31:38 -0300

    search page styles

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ const routes = express.Router()
 const multer = require('./app/middlewares/multer')
 const ProductController = require('./app/controllers/productController')
 const homeController = require('./app/controllers/homeController')
+const searchController = require('./app/controllers/searchController')
 
+// Home
 routes.get('/', homeController.index)
 
+// Products
 routes.get('/produtos/criar', ProductController.create)
 routes.get('/produtos/:id', ProductController.show)
 routes.get('/produtos/:id/editar', ProductController.edit)
@@ -13,6 +16,9 @@ routes.get('/produtos/:id/editar', ProductController.edit)
 routes.post('/produtos', multer.array('photos', 6), ProductController.post)
 routes.put('/produtos', multer.array('photos', 6), ProductController.put)
 routes.delete('/produtos', ProductController.delete)
+
+// Search
+routes.get('/buscar', searchController.index)
 
 //Alias
 routes.get('/anuncios/criar', (req, res) => {
