07c11cbed3445af5acb8f99cc80ce2267a42f807
ricassiocosta <riksiocosta@gmail.com>
2020-08-14 11:05:38 -0300

    searching and filtering products

--- src/app/controllers/searchController.js
+++ src/app/controllers/searchController.js
@@ -3,26 +3,55 @@ const { formatPrice } = require('../../lib/utils')
 
 module.exports = {
   async index(req, res) {
-    let results = await Product.all()
-    const products = results.rows
+    try {
+      let results,
+        params = {  }
+      
+      const { filter, category } = req.query
 
-    if(!products) return res.send("Products not found!")
+      if(!filter) return res.redirect('/')
 
-    async function getImage(productId) {
-      let results = await Product.files(productId)
-      const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
-      return files[0]
+      params.filter = filter
+
+      if(category) {
+        params.category = category
+      }
+
+      async function getImage(productId) {
+        let results = await Product.files(productId)
+        const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+        return files[0]
+      }
+
+      results = await Product.search(params)
+      const productsPromise = results.rows.map(async product =>{
+        product.img = await getImage(product.id)
+        product.price = formatPrice(product.price)
+        product.oldPrice = formatPrice(product.old_price)
+        return product
+      })
+
+      const products = await Promise.all(productsPromise)
+
+      const search = {
+        term: req.query.filter,
+        total: products.length
+      }
+
+      const categories = products.map(product => ({
+        id: product.category_id,
+        name: product.category_name,
+      })).reduce((categoriesFiltered, category) => {
+        const found = categoriesFiltered.some(cat => cat.id == category.id)
+        if(!found)
+          categoriesFiltered.push(category)
+        return categoriesFiltered
+      }, [])
+      
+      return res.render("search/index", { products, search, categories })
+    } catch (err) {
+      console.log(err)
     }
 
-    const productsPromise = products.map(async product => {
-      product.img = await getImage(product.id)
-      product.price = formatPrice(product.price)
-      product.oldPrice = formatPrice(product.old_price)
-      return product
-    }).filter((product, index) => index > 2 ? false : true)
-
-    const lastAdded = await Promise.all(productsPromise)
-
-    return res.render("search/index", { products: lastAdded })
   }
 }
--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -80,5 +80,35 @@ module.exports = {
     return db.query(`
       SELECT * FROM files WHERE product_id = $1
     `, [id])
+  },
+
+  search(params) {
+    const { filter, category } = params
+
+    let query = "",
+        filterQuery = `WHERE`
+
+    if(category) {
+      filterQuery = `${filterQuery}
+        products.category_id = ${category}
+        AND
+      `
+    }
+
+    filterQuery = `
+      ${filterQuery}
+      (products.name ILIKE '%${filter}%'
+      OR products.description ILIKE '%${filter}%')
+    `
+
+    query = `
+      SELECT products.*,
+        categories.name AS category_name
+      FROM products
+      LEFT JOIN categories ON (products.category_id = categories.id)
+      ${filterQuery}
+    `
+
+    return db.query(query)
   }
 }
