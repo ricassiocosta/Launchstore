2f2b0b14a926a1b03989fe15d71d9c896e8fb9f8
ricassiocosta <riksiocosta@gmail.com>
2020-08-24 13:41:29 -0300

    password reset

--- src/app/controllers/sessionController.js
+++ src/app/controllers/sessionController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const crypto = require('crypto')
+const { hash } = require('bcryptjs')
 const mailer = require('../../lib/mailer')
 
 module.exports = {
@@ -63,16 +64,28 @@ module.exports = {
     return res.render('session/password-reset', {token: req.query.token})
   },
 
-  reset(req, res) {
-    const { email, password, passwordRepeat, token } = req.body
+  async reset(req, res) {
+    const user = req.user
+    const { password, token } = req.body
 
     try {
-      
+      const newPassword = await hash(password, 8)
+      await User.update(user.id, {
+        password: newPassword,
+        reset_token: '',
+        reset_token_expires: ''
+      })
 
+      return res.render('session/login', {
+        user: req.body,
+        success: 'Senha atualizada com sucesso!'
+      })
 
     } catch (err) {
       console.error(err)
       return res.render('session/password-reset', {
+        user: req.body,
+        token,
         error: 'Erro inesperado, tente novamente!'
       })
     }
