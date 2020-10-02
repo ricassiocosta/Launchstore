40575f8bc6941891034e8937bfb4e5df34e567f0
ricassiocosta <riksiocosta@gmail.com>
2020-10-02 17:05:15 -0300

    Cart methods implemented

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -16,16 +16,75 @@ const Cart = {
 
     return this
   },
-  addOne(product) {
 
+  addOne(product) {
+    let inCart = this.getCartItem(product.id)
+
+    if(!inCart) {
+      inCart = {
+        product: {
+          ...product,
+          formattedPrice: formatPrice(product.price)
+        },
+        quantity: 0,
+        price: 0,
+        formattedPrice: formatPrice(0)
+      }
+
+      this.items.push(inCart)
+    }
+
+    if(inCart.quantity >= product.quantity) return this
+
+    inCart.quantity++
+    inCart.price = inCart.product.price * inCart.quantity
+    inCart.formattedPrice = formatPrice(inCart.price)
+
+    this.total.quantity++
+    this.total.price = this.product.price * inCart.quantity
+    this.total.formattedPrice = formatPrice(this.total.price)
+
+    return this
   },
 
   removeOne(productId) {
+    const inCart = this.getCartItem(productId)
+    if(!inCart) return this
 
+    inCart.quantity--
+    inCart.price = inCart.product.price * inCart.quantity
+    inCart.formattedPrice = formatPrice(inCart.price)
+
+    this.total.quantity--
+    this.total.price -= inCart.product.price
+    this.total.formattedPrice = formatPrice(this.total.price)
+
+    if(inCart.quantity < 1) {
+      const itemIndex = this.items.indexOf(inCart)
+      this.items.splice(itemIndex, 1)
+      return this
+    }
+
+    return this
   },
 
   delete(productId) {
-    
+    const inCart = this.getCartItem(productId)
+    if(!inCart) return this
+
+    if(this.items.length > 0) {
+      this.total.quantity -= inCart.quantity
+      this.total.price -= (inCart.product.price * inCart.quantity)
+      this.total.formattedPrice = formatPrice(this.total.price)
+    }
+
+    this.items = this.items.filter(item => inCart.product.id != item.product.id)
+    return this
+  },
+
+  getCartItem(productId) {
+    return this.items.find(item => item.product.id == productId)
   }
 }
  
+module.exports = Cart
