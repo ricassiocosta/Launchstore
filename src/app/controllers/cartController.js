ede05e697cbcf4471e65130b17c9f33ee72f9dc4
ricassiocosta <riksiocosta@gmail.com>
2020-10-06 08:42:07 -0300

    adding product to cart

--- src/app/controllers/cartController.js
+++ src/app/controllers/cartController.js
@@ -5,17 +5,28 @@ const LoadProductService = require('../services/LoadProductService')
 module.exports = {
   async index(req, res) {
     try {
-      const product = await LoadProductService.load('product', { where: {id: 1}})
-
       let { cart } = req.session
 
-      cart = Cart.init(cart).addOne(product)
-      console.log(cart.items[0].product.name)
+      cart = Cart.init(cart)
 
       return res.render('cart/index', { cart })
-
     } catch (error) {
       console.error(error)
     }
+  },
+
+  async addOne(req, res) {
+    try {
+      const { id } = req.params
+      const product = await LoadProductService.load('product', { where: {id: id}})
+
+      let { cart } = req.session
+      req.session.cart = Cart.init(cart).addOne(product)
+
+      return res.redirect('/carrinho')
+    } catch (error) {
+      console.error(error)
+    }
+
   }
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,5 +4,6 @@ const routes = express.Router()
 const CartController = require('../app/controllers/cartController')
 
 routes.get('/', CartController.index)
+      .post('/:id/adicionar', CartController.addOne)
 
 module.exports = routes
